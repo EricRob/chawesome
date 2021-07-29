efb555f7dd0d2305b41c8f2ed582eee41d49f64a
Eric Robinson <eric.james.robinson@gmail.com>
2021-07-28 22:38:56 -0700

    Organize legend and graph nodes by category, give each category a shade range within a single color

--- tie_and_travel.py
+++ tie_and_travel.py
@@ -5,13 +5,51 @@ import numpy as np
 import pdb
 import os
 import random
+from colorsys import hsv_to_rgb
 
 
 LEGEND = []
 COLORS = {}
+SCREEN_TO_COLOR = {}
+
 
 # FUNCTIONS
+def sort_screens(*csvs):
+    macros = pd.read_csv('csvs/screen_info.csv')
+    for index, csv_name in enumerate(csvs):
+        times = pd.read_csv(csv_name)
+        for i, row in times.iterrows():
+            category = macros.loc[macros['ID'] == str(row.Screen), 'MACRO'].values
+            if len(category) > 0:
+                category = category[0]
+            else:
+                category = 'UNKNOWN CATEGORY'
+            if category not in COLORS:
+                COLORS[category] = []
+            if row.Screen not in COLORS[category]:
+                COLORS[category].append(row.Screen)
+    hue_increment = 360 // (len(COLORS) + 1)
+    hue = 0
+    for index, category in enumerate(COLORS):
+        COLORS[category].sort()
+        hue += hue_increment*2
+        print(hue)
+        saturation = np.linspace(0.6, 1, num=len(COLORS[category]))
+        value = np.linspace(.6, .90,  num=len(COLORS[category]))
+        print(category)
+        for i, screen in enumerate(COLORS[category]):
+            h = hue/360
+            s = saturation[i]
+            v = value[i]
+
+            # Mult by 255?
+            r, g, b = hsv_to_rgb(h, s, v)
+            # r, g, b = [int(x) for x in (r, g, b)]
+            r, g, b = [int(x*255) for x in (r, g, b)]
+            SCREEN_TO_COLOR[str(screen)] = '#%02X%02X%02X' % (r, g, b)
+
 def tie_plot(*csvs):
+
     for index, csv_name in enumerate(csvs):
         times = pd.read_csv(csv_name)
         times.loc[:, 'end'] = times['end'].apply(lambda x: x - times['start'].min())
@@ -19,35 +57,43 @@ def tie_plot(*csvs):
 
         height = 0.3
 
-        screens = times.Screen.unique()
-        r = lambda: random.randint(0,255)
-        for screen in screens:
-            scr = str(screen)
-            if scr not in COLORS:
-                COLORS[scr] = '#%02X%02X%02X' % (r(),r(),r())
+        # screens = times.Screen.unique()
+        # r = lambda: random.randint(0,255)
+        # for screen in screens:
+        #     scr = str(screen)
+        #     if scr not in COLORS:
+        #         COLORS[scr] = '#%02X%02X%02X' % (r(),r(),r())
         if index == 4:
             continue
         for i, row in times.iterrows():
             screen = str(int(row.Screen))
-            if screen not in LEGEND:
-                plt.barh(index, row.end, color=COLORS[screen], label=screen, height=height)
-                LEGEND.append(screen)
-            else:
-                plt.barh(index, row.end, color=COLORS[screen], height=height)
+            # if screen not in LEGEND:
+            #     plt.barh(index, row.end, color=SCREEN_TO_COLOR[screen], label=screen, height=height)
+            # else:
+            plt.barh(index, row.end, color=SCREEN_TO_COLOR[screen], height=height)
+
+    for category in COLORS:
+        for screen in COLORS[category]:
+            LEGEND.append(plt.Line2D([0], [0], color=SCREEN_TO_COLOR[str(screen)], lw=3, label=str(screen)))
 
     plt.plot()
     plt.xlabel("Elapsed time")
     plt.ylabel("Subject")
     plt.title("Helen Screen switches")
     plt.savefig(os.path.join('data', 'tie_plot.png'))
-    plt.legend(loc='center', fancybox=True, shadow=True, ncol=3)
+    plt.legend(handles=LEGEND, loc='center', fancybox=True, shadow=True, ncol=3)
     plt.savefig(os.path.join('data', 'legend.png'))
+    plt.clf()
 
 def travel_graph(*csvs):
     fig, ax = plt.subplots(3, 3, num=1)
     fig.set_size_inches(12, 12)
     G = nx.DiGraph()
     edges = {}
+
+    for category in COLORS:
+        for screen in COLORS[category]:
+            G.add_node(screen)
 
     for index, csv_name in enumerate(csvs):
         label = csv_name.split('.')[0].split('/')[-1]
@@ -59,7 +105,6 @@ def travel_graph(*csvs):
         a = None
         for i, row in times.iterrows():
             b = int(row.Screen)
-            G.add_node(b)
             if a:
                 edges[label].append((a, b))
             a = b
@@ -68,7 +113,7 @@ def travel_graph(*csvs):
         # G.add_edges_from(edges)
     color_arr = []
     for node in G.nodes():
-        color_arr.append(COLORS[str(node)])
+        color_arr.append(SCREEN_TO_COLOR[str(node)])
 
     pos = nx.circular_layout(G)
     # if gtype == 'spring':
@@ -90,10 +135,17 @@ def travel_graph(*csvs):
         axis.set_title(edge)
         plt.sca(axis)
         color = '#%02X%02X%02X' % (r(),r(),r())
+        color = '#181818'
         nx.draw_networkx_edges(G, pos, ax=axis, edgelist=edges[edge], edge_color=color, arrows=True)
         nx.draw_networkx_nodes(G, pos, ax=axis, node_color=color_arr, node_size=100)
-        nx.draw_networkx_labels(G, pos, ax=axis, )
+        # nx.draw_networkx_labels(G, pos, ax=axis, )
         plt.title(edge)
+    graph_legend = []
+    for category in COLORS:
+        graph_legend.append(plt.Line2D([0], [0], color=SCREEN_TO_COLOR[str(COLORS[category][-1])], lw=3, label=category))
+    axis = ax[2][2]
+    axis.axis('off')
+    axis.legend(handles=graph_legend, loc='center', fancybox=True, shadow=True)
     plt.savefig(os.path.join('data', 'circ_graphs.png'))
 
 
@@ -110,7 +162,7 @@ csvs = []
 for i in range(8):
     csvs.append(os.path.join('csvs', f'helen_{i + 1}.csv'))
 
-
+sort_screens(*csvs)
 tie_plot(*csvs)
 travel_graph(*csvs)
 
